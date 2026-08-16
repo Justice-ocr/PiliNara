f6c2266f996c257aa16895428c8284602fa4c540
Justice-ocr <yueyutong311@gmail.com>
2026-08-16 11:34:07 +0800

    fix(main): keep recommendation list mutable for tag filtering

--- lib/http/video.dart
+++ lib/http/video.dart
@@ -156,7 +156,7 @@ abstract final class VideoHttp {
     );
 
     if (res.data['code'] == 0) {
-      final list = <RcmdVideoItemAppModel>[];
+      List<RcmdVideoItemAppModel> list = <RcmdVideoItemAppModel>[];
       final bool removeBlockedRcmd = Pref.removeBlockedRcmd;
       for (final i in res.data['data']['items']) {
         final upMid = safeToInt(i['args']?['up_id']);
