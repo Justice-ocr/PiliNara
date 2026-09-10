c5369b62d5ba781d6667b69ac973b9535be02ac0
ci <example@example.com>
2026-09-10 15:45:10 +0800

    fix: support fork GetX in silent list fill

--- lib/utils/extension/rx_ext.dart
+++ lib/utils/extension/rx_ext.dart
@@ -2,9 +2,10 @@ import 'package:get/get_rx/src/rx_types/rx_types.dart' show RxList;
 
 extension RxListExt<E> on RxList<E> {
   void fillRangeOnly(int start, int end, [E? fill]) {
-    E value = fill as E;
+    E fillValue = fill as E;
+    final items = value;
     for (int i = start; i < end; i++) {
-      rawValue[i] = value;
+      items[i] = fillValue;
     }
   }
 }
